ac23a1bf02ae51d1a8154a02156216ea2db2a8a7
aleyananda <thisisurcafeine@gmail.com>
2024-10-13 19:58:32 +0700

    aliyananda risyahputri

---
+++ app/src/main/java/com/example/myapplication/pengajuansptjm.kt
@@ -0,0 +1,31 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Pengajuansptjm : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_pengajuansptjm)
+
+        val sptjmActBtn  = findViewById<ImageButton>(R.id.btnBackSPTJM)
+        sptjmActBtn.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+}
