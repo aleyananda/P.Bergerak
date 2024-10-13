7e5fad0f27d8915e05da9e0be4c6be655cd1f7d6
nailaraihana <nailaraihana55511@gmail.com>
2024-10-13 20:45:42 +0700

    Naila Raihana

--- app/src/main/java/com/example/myapplication/HomeActivity.kt
+++ app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -1,10 +1,13 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +19,15 @@ class HomeActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val hometotemplatebutton: ImageButton = findViewById(R.id.templatebutton)
+        hometotemplatebutton.setOnClickListener {
+            // Intent to open HomeActivity
+            val intent = Intent(this@HomeActivity, Template::class.java)
+            startActivity(intent)
+        }
     }
 }
+
+
+
