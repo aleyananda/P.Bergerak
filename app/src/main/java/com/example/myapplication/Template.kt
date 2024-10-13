cbae677f695c69164bf27d2357d780fb89bd4b99
Jodi16 <jodiii.pratamaaa@gmail.com>
2024-10-13 15:13:09 +0700

    Jodi update click beranda template

--- app/src/main/java/com/example/myapplication/Template.kt
+++ app/src/main/java/com/example/myapplication/Template.kt
@@ -1,20 +1,33 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
 
 class Template : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_template)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Mencari button berdasarkan id dan menambahkan OnClickListener
+        val button = findViewById<Button>(R.id.btn_home)
+        button.setOnClickListener {
+            // Membuat intent untuk berpindah ke HomeActivity
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
         }
     }
 }
