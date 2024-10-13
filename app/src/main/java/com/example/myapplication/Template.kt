16c8a7e97fdda9fdc3d7e50b711a30f0753333b6
Jodi16 <jodiii.pratamaaa@gmail.com>
2024-10-13 15:27:37 +0700

    Jodi update click beranda template

--- app/src/main/java/com/example/myapplication/Template.kt
+++ app/src/main/java/com/example/myapplication/Template.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.Button
+import android.widget.ImageButton
 
 class Template : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -25,6 +26,14 @@ class Template : AppCompatActivity() {
         // Mencari button berdasarkan id dan menambahkan OnClickListener
         val button = findViewById<Button>(R.id.btn_home)
         button.setOnClickListener {
+            // Membuat intent untuk berpindah ke HomeActivity
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Mencari ImageButton berdasarkan id dan menambahkan OnClickListener
+        val imgButton = findViewById<ImageButton>(R.id.imgbtn_home)
+        imgButton.setOnClickListener {
             // Membuat intent untuk berpindah ke HomeActivity
             val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
