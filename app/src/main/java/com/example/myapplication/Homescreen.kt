e4b7e507a9540a1a1375db66e491bf9871ff2f6f
nailaraihana <nailaraihana55511@gmail.com>
2024-10-13 22:31:47 +0700

    Naila Raihana

--- app/src/main/java/com/example/myapplication/Homescreen.kt
+++ app/src/main/java/com/example/myapplication/Homescreen.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -8,17 +9,20 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.ImageButton
 
+
 class Homescreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_homescreen)
 
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
 
         // Find the ImageButton and set the click listener
         val homeToMainButton: ImageButton = findViewById(R.id.hometomain)
