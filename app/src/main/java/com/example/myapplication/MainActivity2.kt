ebaba421209c2757c7dfe8d42307fdce423cf3a9
nailaraihana <nailaraihana55511@gmail.com>
2024-10-14 09:24:31 +0700

    Naila Raihana

--- app/src/main/java/com/example/myapplication/MainActivity2.kt
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -10,10 +10,6 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity2 : AppCompatActivity() {
 
-    lateinit var expandableListView: ExpandableListView
-    var header: MutableList<String> = ArrayList()
-    val childItem: MutableList<MutableList<String>> = ArrayList()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
